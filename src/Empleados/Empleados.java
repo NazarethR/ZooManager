37d41a2d6550937e3e821759e3805a2e34a0e56e
Nazareth Ramirez <nazareth2006ramirez@gmail.com>
2025-06-24 12:13:37 -0600

    se modifico la clase empleados
    se elimino un import y se hizo hija de la clase persona

--- src/Empleados/Empleados.java
+++ src/Empleados/Empleados.java
@@ -3,42 +3,17 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Empleados;
+import Personas.Persona;
 import java.time.LocalDate;
-import java.time.Period;
-
-public class Empleados {
-   
-    protected String Cedula;
-    protected String Nombre;
-    protected LocalDate FechaNacimiento;
-    protected String Telefono;
+/**
+ *
+ * @author Student
+ */
+public class Empleados extends Persona {
     protected double SalarioMen;
-
-    public String getCedula() {
-        return Cedula;
-    }
-
-    public String getNombre() {
-        return Nombre;
-    }
-
-    public LocalDate getFechaNacimiento() {
-        return FechaNacimiento;
-    }
-
-    public String getTelefono() {
-        return Telefono;
-    }
 
     public double getSalarioMen() {
         return SalarioMen;
-    }
-
-    public void setTelefono(String Telefono) {
-        if(Telefono!=null && Telefono.matches("^\\d{2}-\\d{2}-\\d{2}-\\d{2}")){
-        this.Telefono = Telefono;
-        }
-       
     }
 
     public void setSalarioMen(double SalarioMen) {
@@ -47,22 +22,17 @@ public class Empleados {
         }
     }
 
-    public Empleados(String Cedula, String Nombre, LocalDate FechaNacimiento, String Telefono, double SalarioMen) {
-        this.Cedula = Cedula;
-        this.Nombre = Nombre;
-        if(FechaNacimiento!=null && !FechaNacimiento.isAfter(LocalDate.now()) && 18 < Period.between(FechaNacimiento,LocalDate.now()).getYears()){
-            this.FechaNacimiento = FechaNacimiento;
-        }
-        if(Telefono!=null && Telefono.matches("^\\d{2}-\\d{2}-\\d{2}-\\d{2}")){
-            this.Telefono = Telefono;
-        }
-        if(SalarioMen>=300000){
+    public Empleados(double SalarioMen, String cedula, String NombreComp, LocalDate FechaNacimiento, String Telefono) {
+        super(cedula, NombreComp, FechaNacimiento, Telefono);
+        if(SalarioMen>=300000)
             this.SalarioMen = SalarioMen;
-        }
     }
 
     @Override
     public String toString() {
-        return "Empleado{" + "Cedula=" + Cedula + ", Nombre=" + Nombre+ ", FechaNacimiento=" + FechaNacimiento + ", Telefono=" + Telefono + ", SalarioMen=" + SalarioMen + '}';
+        return "Empleado{" + "cedula=" + cedula + ", NombreComp=" + NombreComp + ", FechaNacimiento=" + FechaNacimiento + ", Telefono=" + Telefono + ", SalarioMen=" + SalarioMen + '}';
     }
+    
+    
+    
 }
