7c1cfdaab7cefbed6fb44a9662228dc3d7fa66c7
Nazareth Ramirez <nazareth2006ramirez@gmail.com>
2025-06-24 12:38:08 -0600

    Se modifico la clase animal
    se modifico sus validaciones

--- src/Animal/Animales.java
+++ src/Animal/Animales.java
@@ -3,53 +3,61 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Animal;
+import Utils.UtilDate;
 import java.time.LocalDate;
-
+/**
+ *
+ * @author Student
+ */
 public class Animales {
-     private String Identificacion;
-    private String Nombre;
+    private String idAnimal;
+    private String NombreAnimal;
     private String Especie;
-    private LocalDate Fecha_de_nacimiento;
-   protected  Zona zonas;
+    private LocalDate FechaNacimiento;
+    private Zona Zone;
 
-    public String getIdentificacion() {
-        return Identificacion;
+    public String getIdAnimal() {
+        return idAnimal;
     }
 
-    public String getNombre() {
-        return Nombre;
+    public String getNombreAnimal() {
+        return NombreAnimal;
     }
 
     public String getEspecie() {
         return Especie;
     }
 
-    public LocalDate getFecha_de_nacimiento() {
-        return Fecha_de_nacimiento;
+    public LocalDate getFechaNacimiento() {
+        return FechaNacimiento;
     }
 
-   
-    public Zona getZonas() {
-        return zonas;
+    public Zona getZooZone() {
+        return Zone;
     }
 
-    public void setZonas(Zona zonas) {
-        this.zonas = zonas;
+    public int ObtnerEdad(){
+        return UtilDate.CalcuarEdad(FechaNacimiento);
+    }
+    
+    public void setZooZone(Zona Zone) {
+        this.Zone = Zone;
     }
 
-    public Animales(String Identificacion, String Nombre, String Especie, LocalDate Fecha_de_nacimiento) {
-       if( Identificacion!=null && Identificacion.matches("^\\w{1}-[\\d]{4}$")){
-            this.Identificacion = Identificacion;}
-        
-        this.Nombre = Nombre;
+    public Animales(String idAnimal, String NombreAnimal, String Especie,LocalDate FechaNacimiento) {
+        if( idAnimal!=null && idAnimal.matches("^\\w{1}-[\\d]{4}$")){
+            this.idAnimal = idAnimal;
+        }
+        this.NombreAnimal = NombreAnimal;
         this.Especie = Especie;
-        if(Fecha_de_nacimiento!=null && !Fecha_de_nacimiento.isAfter(LocalDate.now())){
-        this.Fecha_de_nacimiento = Fecha_de_nacimiento;}
-        this.zonas = Zona.CONSERVACION_Y_RESCATE;
+        if(FechaNacimiento!=null && UtilDate.ValidarFechasFut(LocalDate.now())){
+            this.FechaNacimiento = FechaNacimiento;
+        }
+        this.Zone = Zona.CONSERVACION_Y_RESCATE;
     }
 
     @Override
     public String toString() {
-        return "Registro_de_animales{" + "Identificacion=" + Identificacion + ", Nombre=" + Nombre + ", Especie=" + Especie + ", Fecha_de_nacimiento=" + Fecha_de_nacimiento + ", zonas=" + zonas + '}';
+        return "Animal{" + "idAnimal=" + idAnimal + ", NombreAnimal=" + NombreAnimal + ", Especie=" + Especie + ", FechaNacimiento=" + FechaNacimiento + ", zooZone=" + Zone + '}';
     }
 }
