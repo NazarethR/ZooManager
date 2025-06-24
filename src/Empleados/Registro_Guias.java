b79dd9f2fe71cf81dd75846209b53c91b50a7eff
Nazareth Ramirez <nazareth2006ramirez@gmail.com>
2025-06-24 14:34:37 -0600

    le borre unas cosilas
    borre lo que esta encima de tostring, u unos comentarios

--- src/Empleados/Registro_Guias.java
+++ src/Empleados/Registro_Guias.java
@@ -3,23 +3,22 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package Empleados;
+import Recorridos.Recorrido;
 import Animal.Animales;
 import java.time.LocalDate;
-import java.util.Scanner;
-
+import Visitantes.Visitante;
 
 
 public class Registro_Guias extends Empleados {
-    Scanner scan = new Scanner(System.in);
     private String[] Idiomas;
-    private String[] RegistroRecorrido;
+    private Recorrido RegistroRecorrido[];
     
 
     public String[] getIdiomas() {
         return Idiomas;
     }
 
-    public String[] getRegistroRecorrido() {
+    public Recorrido[] getRegistroRecorrido() {
         return RegistroRecorrido;
     }
     
@@ -27,34 +26,37 @@ public class Registro_Guias extends Empleados {
         this.Idiomas = Idiomas;
     }
     
-    public void RecorridoGuiadoVisitantes (){
-        for (int i=0;i<200;i++){
-            if (RegistroRecorrido[i]==null){
-                RegistroRecorrido[i]= "Se le hizo recorrido a: "+scan;
+    
+    public void RecorridoGuiadoVisitantes (Visitante Visitantes[]){
+        for (int i = 0; i < 50; i++) {
+            if(RegistroRecorrido[i]==null){
+                RegistroRecorrido[i]=new Recorrido(this,Visitantes);
                 break;
             }
         }
     }
            
-    public void InforAnimalVisto(Animales Identificacion){
-        for(int i=0;i<200;i++){
-            if (RegistroRecorrido[i]==null){
-                RegistroRecorrido[i]= "Información del Animal observado: "+Identificacion;
+    public String InforAnimalVisto(Animales idAnimal){
+        return "Se dió información de: "+ idAnimal;
+    }
+
+    public void añadirIdiomas(String language){
+        for (int i = 0; i < 15; i++) {
+            if(Idiomas[i]==null){
+                Idiomas[i]=language;
                 break;
             }
         }
     }
-
+    
     public Registro_Guias(String[] Idiomas, String[] RegistroRecorrido, double SalarioMen, String cedula, String NombreComp, LocalDate FechaNacimiento, String Telefono) {
         super(SalarioMen, cedula, NombreComp, FechaNacimiento, Telefono);
         this.Idiomas = new String[25];
-        this.RegistroRecorrido= new String[200];
+        this.RegistroRecorrido= new Recorrido[200];
     }
-    
-    
 
-    @Override
+    
     public String toString() {
         return "Guías{" + "Idiomas=" + Idiomas + '}';
-    }
+    } 
 }
